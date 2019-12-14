3245ef814af05f5ef059be9df951b6dc4022455a
Matthew Geiger <mgeiger@unca.edu>
2019-12-14 12:47:43 -0500

    [3.3.2] leave out the note about the differently scaled y axes

--- index.js
+++ index.js
@@ -179,6 +179,7 @@ let main = () => {
           cddAxisSvg.call(yAxisCdd.ticks(8))
           hddAxisSvg.call(yAxisHdd.ticks(5))
           //hideElements([startYearLine])
+          /* Remove the note about two y axes for 3.3.2
           let otherChartNote = appendChartNote(chart,
             "The two y axes show different scales"
           )
@@ -186,6 +187,7 @@ let main = () => {
           otherChartNote.attr("transform",
             `translate(80, 625)`
           )
+          */
           setTimeout(() => {
             hideElements([ d3.selectAll("svg") ]) 
           }, 5000) //3000
